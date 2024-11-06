02583741b08c6ebca791d3b1bca739c3717d969c
brandon <jofrebrann@gmail.com>
2024-11-06 20:47:05 -0300

    actualizacion

--- src/clases/tareas-lista.class.js
+++ src/clases/tareas-lista.class.js
@@ -1,22 +1,34 @@
 export class TareaLista{
     constructor(){
-        this.tareas = [];
+        // this.tareas = [];
+        // this.guardarLocalStorage();
+        this.cargarLocalStorage();
     }
     nuevaTarea(tarea){
         this.tareas.push(tarea);
+        this.guardarLocalStorage();
     }
     eliminarTarea(id){
-
+        this.tareas = this.tareas.filter(elemento => elemento.id != id);
+        this.guardarLocalStorage();
     }
     marcarCompletado(id){
         for(const elemento of this.tareas){
             if(elemento.id == id){
                 elemento.completado = !elemento.complatado;
+                this.guardarLocalStorage();
                 break;
             }
         }
     }
     eliminarCompletados(){
-        
+        this.tareas = this.tareas.filter(elemento => !elemento.completado);
+        this.guardarLocalStorage();
+    }
+    guardarLocalStorage(){
+        localStorage.setItem('tarea',JSON.stringify(this.tareas))
+    }
+    cargarLocalStorage(){
+            this.tareas =(localStorage.getItem('tarea')) ? JSON.parse(localStorage.getItem('tarea')) : [];
     }
 }
--- src/index.js
+++ src/index.js
@@ -5,8 +5,5 @@ import { crearTareaHtml } from './js/funciones'
 export const listaTarea = new TareaLista()
 const tarea = new Tarea("Practicar webpack")
 
-listaTarea.nuevaTarea(tarea);
-
-crearTareaHtml(tarea);
-console.log(listaTarea);
+listaTarea.tareas.forEach((elemento)=>{crearTareaHtml(elemento)});
 
